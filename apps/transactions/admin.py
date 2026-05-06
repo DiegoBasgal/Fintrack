ae3296c337977ea57d30649d1cdfa64daecbc0b6
Diego Basgal <diegobasgal@hotmail.com>
2026-05-06 14:08:47 -0300

    Models + Admin with categories and transactions

--- apps/transactions/admin.py
+++ apps/transactions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Transaction, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'type', 'category', 'date']
+    list_filter = ['type', 'category']
+    search_fields = ['description']
